51e86eb517421b9d6d7de8add8ae6cd7e2c5f2bb
Isabella Costabile <costabileisa@gmail.com>
2022-12-04 16:32:19 -0500

    commit tree 1cb56b1cbd8089e59ad91260b715d319755707c5

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = array => array.slice(0, 2);
+
+const returnLastTwoDrivers = array => array.slice(-2);
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return multiplier => fare * multiplier;
+};
+
+function fareDoubler(fare) {
+    // return createFareMultiplier(fare)(2)
+    const double = createFareMultiplier(fare);
+    return double(2);
+};
+
+function fareTripler(fare) {
+    // return createFareMultiplier(fare)(3)
+    const triple = createFareMultiplier(fare);
+    return triple(3);
+};
+
+function selectDifferentDrivers(array, fn) {
+    return fn(array);
+};
